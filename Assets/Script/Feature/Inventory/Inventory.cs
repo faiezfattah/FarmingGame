d457b68ead20b5b44327c66f69031bb9ea41c47a
faiezfattah <faiezfattah2005@gmail.com>
2025-07-21 19:06:11 +0700

    whatever this is

--- Assets/Script/Feature/Inventory/Inventory.cs
+++ Assets/Script/Feature/Inventory/Inventory.cs
@@ -4,20 +4,54 @@ using R3;
 using UnityEngine.UIElements;
 using Script.Feature.Input;
 using System;
+using ObservableCollections;
+using System.Collections.Generic;
+using Script.Core.Model.Item;
+using TriInspector;
+using System.Linq;
 public class Inventory : MonoBehaviour {
     [SerializeField] private UIDocument uIDocument;
-    private IInventoryRegistry _inventoryRegistry;
+    private InventoryRegistry _inventoryRegistry;
     private IDisposable _subscription;
-    [Inject] public void Construct (IInventoryRegistry inventoryRegistry, InputProcessor inputProcessor) {
+    private DisposableBag _bag = new();
+    private ISynchronizedView<KeyValuePair<int, PackedItemContext>, KeyValuePair<int, PackedItemContext>> view;
+
+    [Inject]
+    public void Construct(InventoryRegistry inventoryRegistry, InputProcessor inputProcessor) {
         _inventoryRegistry = inventoryRegistry;
-        _subscription = inputProcessor.InventoryEvent.Subscribe(_ => ToggleInventory()) ;
+        _subscription = inputProcessor.InventoryEvent.Subscribe(_ => ToggleInventory());
+        inventoryRegistry.inventory2.ObserveChanged().Subscribe(HandleChange).AddTo(ref _bag);
+        view = _inventoryRegistry.inventory2
+            .CreateView((val) => val)
+            .AddTo(ref _bag);
+        view.AttachFilter(
+                (a, b) => true
+            );
+
+        // first 5
+        var first5ItemsView = _inventoryRegistry.inventory2
+            .CreateView(
+                kvp => {
+                    return kvp.Key < 5;
+                }
+            )
+            .AddTo(ref _bag);
+
+        view = _inventoryRegistry.inventory2
+            .CreateView(kvp => kvp)
+            .AddTo(ref _bag);
+    }
+    private void HandleChange(CollectionChangedEvent<KeyValuePair<int, PackedItemContext>> _) {
+        foreach (var (i, value) in view.OrderBy(kvp => kvp.Key)) {
+            Debug.Log($"{i} : {value.ItemContext.BaseData.name} {value.Count}");
+        }   
+    }
+    [Button]
+    private void AddToInventory2(ItemData itemData, int count, int index) {
+        _inventoryRegistry.inventory2.Add(index, new PackedItemContext(itemData.CreateBaseContext(), count));
     }
     private void Start() {
         uIDocument.enabled = false;
-
-        // var root = uIDocument.rootVisualElement;
-        // var inventory = root.Query<InventoryDisplay>().First();
-        // inventory.SetInventoryBinding(_inventoryRegistry);
     }
     private void ToggleInventory() {
         uIDocument.enabled = !uIDocument.enabled;
--- Assets/Script/Feature/Inventory/InventoryRegistry.cs
+++ Assets/Script/Feature/Inventory/InventoryRegistry.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using ObservableCollections;
 using R3;
 using Script.Core.Interface;
@@ -8,6 +9,42 @@ public class InventoryRegistry : IInventoryRegistry {
     public ObservableList<PackedItemContext> inventory = new();
     IReadOnlyObservableList<PackedItemContext> IInventoryRegistry.InventoryRegistry => inventory;
 
+    // new system
+    public ObservableDictionary<int, PackedItemContext> inventory2 = new();
+    public void Add(int index, PackedItemContext packedItem) {
+        if (inventory2.TryGetValue(index, out var existing) &&
+            existing.ItemContext.BaseData == packedItem.ItemContext.BaseData) {
+            existing.Count.Value += packedItem.Count.Value;
+            return;
+        }
+        
+        inventory2[index] = packedItem;
+        inventory2.OrderBy(kvp => kvp.Key);
+        
+    }
+    public void Add(int index, ItemContext itemContext, int value = 1) {
+        if (inventory2.TryGetValue(index, out var existing) &&
+            existing.ItemContext.BaseData == itemContext.BaseData) {
+            existing.Count.Value += value;
+            return;
+        }
+        
+        inventory2[index] = new PackedItemContext(itemContext.BaseData.CreateBaseContext(), value);
+        inventory2.OrderBy(kvp => kvp.Key);
+        
+    }
+    public void Add(int index, ItemData itemData, int value = 1) {
+        if (inventory2.TryGetValue(index, out var existing) &&
+            existing.ItemContext.BaseData == itemData) {
+            existing.Count.Value += value;
+            return;
+        }
+        
+        inventory2[index] = new PackedItemContext(itemData.CreateBaseContext(), value);
+        inventory2.OrderBy(kvp => kvp.Key);
+        
+    }
+    
     // toolbar
     public ObservableList<ToolContext> toolbarRegistry = new();
     public IReadOnlyObservableList<ToolContext> ToolRegistry => throw new System.NotImplementedException();
